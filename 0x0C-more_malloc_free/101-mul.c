effa9166e2d3c9e817511e39ed1b08821c6a85a3
Ikeseny0 <Ikesenyo.im@gmail.com>
2023-03-16 19:18:43 -0700

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,7 +4,6 @@
 #include <ctype.h>
 
 /**
- * _is_zero - determines if any number is zero
  * @argv: argument vector.
  *
  * Return: no return.
@@ -35,7 +34,6 @@ void _is_zero(char *argv[])
 }
 
 /**
- * _initialize_array - set memery to zero in a new array
  * @ar: char array.
  * @lar: length of the char array.
  *
@@ -52,7 +50,6 @@ char *_initialize_array(char *ar, int lar)
 }
 
 /**
- * _checknum - determines length of the number
  * and checks if number is in base 10.
  * @argv: arguments vector.
  * @n: row of the array.
@@ -74,7 +71,6 @@ int _checknum(char *argv[], int n)
 }
 
 /**
- * main - Entry point.
  * program that multiplies two positive numbers.
  * @argc: number of arguments.
  * @argv: arguments vector.
